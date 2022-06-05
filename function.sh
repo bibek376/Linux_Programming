7d7dacd50b142d3cb24f1987f893815f8f1c2004
bibek376 <cu.18bcs6728@gmail.com>
2022-06-05 23:51:23 +0545

    Functions In Linux

---
+++ function.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Syntax For Function
+
+: '
+function name(){
+	commands
+}
+
+OR 
+
+name(){
+	commands
+}
+
+
+function call style
+
+name()
+
+: '
+
+function hello(){
+        echo "Hello,World"
+}
+
+hello
+
+
+
