676c7d04dd6a488ccdac870ec5b3e400fa14f995
bibek376 <cu.18bcs6728@gmail.com>
2022-06-17 22:46:12 +0545

    Mail File Modification

--- disk_alert.sh
+++ disk_alert.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
-THRESHOLD=15 
 
+CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
+THRESHOLD=80
 
 if [[ $CURRENT -gt $THRESHOLD ]]
 then
-	mail -s "Disk Space Alert"bibekrawat123@gmail.com <<< Your root partition remaining free space is critically low. Used: $CURRENT%
+        mail -s 'Disk Space Alert!!' cu.18bcs6728@gmail.com <<< "Your root partition remaining free space is critically low.Used:$CURRENT%"
+	echo "Please Check Your EMail"
+else
+        echo "All Good!!!"
 fi
+
 
 
 
